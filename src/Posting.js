176a0f7f8263db93d485cef156bb25ac78d34b4d
Daniel Fernandez <mdanif9728@gmail.com>
2024-06-03 21:20:41 -0700

    create Posting.js

--- src/Posting.js
+++ src/Posting.js
@@ -1,6 +1,21 @@
+// Import components
+import { postings } from "./postings"
+
 function Posting() {
   return (
-    <h2>Positions</h2>
+    <div>
+      <h2>Positions</h2>
+      {postings.map(posting => {
+        return (
+          <div>
+            <img src={posting.imageURL} alt="image" />
+            <h2>{posting.price}</h2>
+            <h3>{posting.title}</h3>
+            <h4>{posting.description}</h4>
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
